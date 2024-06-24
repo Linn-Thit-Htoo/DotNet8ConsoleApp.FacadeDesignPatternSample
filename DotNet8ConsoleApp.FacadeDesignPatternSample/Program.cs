56d545e183a57b8b76af46609b5365ea5e92f3cf
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 18:34:34 +0630

    Format Code

--- DotNet8ConsoleApp.FacadeDesignPatternSample/Program.cs
+++ DotNet8ConsoleApp.FacadeDesignPatternSample/Program.cs
@@ -1,44 +1,47 @@
-﻿public class Program
+﻿namespace DotNet8ConsoleApp.FacadeDesignPatternSample
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Facade facade = new();
-        facade.PerformOperation();
-    }
-}
-
-public class Facade
-{
-    private readonly SubSystemA _subSystemA;
-    private readonly SubSystemB _subSystemB;
-    private readonly SubSystemC _subSystemC;
-
-    public Facade()
-    {
-        _subSystemA = new();
-        _subSystemB = new();
-        _subSystemC = new();
+        public static void Main(string[] args)
+        {
+            Facade facade = new();
+            facade.PerformOperation();
+        }
     }
 
-    public void PerformOperation()
+    public class Facade
     {
-        _subSystemA.OperationA();
-        _subSystemB.OperationB();
-        _subSystemC.OperationC();
+        private readonly SubSystemA _subSystemA;
+        private readonly SubSystemB _subSystemB;
+        private readonly SubSystemC _subSystemC;
+
+        public Facade()
+        {
+            _subSystemA = new();
+            _subSystemB = new();
+            _subSystemC = new();
+        }
+
+        public void PerformOperation()
+        {
+            _subSystemA.OperationA();
+            _subSystemB.OperationB();
+            _subSystemC.OperationC();
+        }
     }
-}
 
-public class SubSystemA
-{
-    public void OperationA() => Console.WriteLine("Operation A in Sub System A");
-}
+    public class SubSystemA
+    {
+        public void OperationA() => Console.WriteLine("Operation A in Sub System A");
+    }
 
-public class SubSystemB
-{
-    public void OperationB() => Console.WriteLine("Operation B in Sub System B");
-}
+    public class SubSystemB
+    {
+        public void OperationB() => Console.WriteLine("Operation B in Sub System B");
+    }
 
-public class SubSystemC
-{
-    public void OperationC() => Console.WriteLine("Operation C in Sub System C");
+    public class SubSystemC
+    {
+        public void OperationC() => Console.WriteLine("Operation C in Sub System C");
+    }
 }
