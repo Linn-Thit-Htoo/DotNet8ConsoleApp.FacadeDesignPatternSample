017678adf9f375dcb12bfc86ff9cbf5771189634
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 18:32:44 +0630

    Add project files.

---
+++ DotNet8ConsoleApp.FacadeDesignPatternSample/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Facade facade = new();
+        facade.PerformOperation();
+    }
+}
+
+public class Facade
+{
+    private readonly SubSystemA _subSystemA;
+    private readonly SubSystemB _subSystemB;
+    private readonly SubSystemC _subSystemC;
+
+    public Facade()
+    {
+        _subSystemA = new();
+        _subSystemB = new();
+        _subSystemC = new();
+    }
+
+    public void PerformOperation()
+    {
+        _subSystemA.OperationA();
+        _subSystemB.OperationB();
+        _subSystemC.OperationC();
+    }
+}
+
+public class SubSystemA
+{
+    public void OperationA() => Console.WriteLine("Operation A in Sub System A");
+}
+
+public class SubSystemB
+{
+    public void OperationB() => Console.WriteLine("Operation B in Sub System B");
+}
+
+public class SubSystemC
+{
+    public void OperationC() => Console.WriteLine("Operation C in Sub System C");
+}
